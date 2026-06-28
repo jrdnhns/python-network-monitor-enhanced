ef7a075baffb17b65908c536f585a16fcbb1db47
jrdnhns <jordanhanes07@yahoo.com>
2026-06-28 14:17:40 -0400

    Add files via upload
    Added updated Folder

---
+++ Project_8/9-1_python-service-health-monitor/service_health_monitor.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import subprocess
+import time
+
+LOG_FILE = "monitoring.log"
+
+devices = {
+    "Google DNS": "8.8.8.8",
+    "Cloudflare DNS": "1.1.1.1",
+    "Default Gateway": "10.0.0.1"
+}
+
+while True:
+    print("\n-- Network Check --")
+
+    for name, target in devices.items():
+        result = subprocess.run(["ping", "-c", "2", target], stdout=subprocess.DEVNULL)
+
+        status = "UP" if result.returncode == 0 else "DOWN"
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f"{timestamp} - {name} ({target}) - {status}"
+	print(message)
+
+	with open(LOG_FILE, "a") as log:
+    log.write(message + "\n")
+
+
+
+    time.sleep(10)
